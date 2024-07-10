3bf6ffb4abcdc2ef5161b53e4e4116aa5b6f1631
SagalBase <142554246+SagalBase@users.noreply.github.com>
2024-07-10 09:06:56 +0800

    Add files via upload

---
+++ barh.py
@@ -0,0 +1,12 @@
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+import numpy as np
+mpl.rcParams['font.sans-serif']=['SimHei']
+mpl.rcParams['axes.unicode_minus']=False
+x=[1,2,3,4,5,6,7,8]
+y=[3,1,4,5,8,9,7,2]
+plt.barh(x,y,align="center",color='c',tick_label=["q","a","c","e","r","j","b","p"],hatch='/')
+plt.xlabel("箱子编号")
+plt.ylabel("箱子重量(kg)")
+
+plt.show()
